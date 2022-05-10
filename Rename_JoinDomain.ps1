4e2fa7aafed5fa49c10d171a47832c8b97d5db6c
apdarjan <105303178+apdarjan@users.noreply.github.com>
2022-05-10 11:25:42 -0400

    Add files via upload

---
+++ Rename_JoinDomain.ps1
@@ -0,0 +1,12 @@
+﻿$pcname = read-host 'New name of computer: '
+$Domain = 'tnet.tti-ecm.com'
+$Credential = Get-Credential
+Rename-Computer $pcname
+
+if ($pcname -match 'TNNB') {
+    Add-Computer -Domain $Domain -OUPath 'OU=Laptops,OU=User Computers,OU=Devices,OU=TNet Ottawa,DC=tnet,DC=tti-ecm,DC=com' -NewName $pcname -Credential $Credential -Restart -Force
+    }
+elseif ($pcname -match 'TNWS') {
+    Add-Computer -Domain $Domain -OuPath 'OU=TPM Desktops,OU=Desktops,OU=User Computers,OU=Devices,OU=TNet Ottawa,DC=tnet,DC=tti-ecm,DC=com' -NewName $pcname -Credential $Credential -Restart -Force
+}
+    
